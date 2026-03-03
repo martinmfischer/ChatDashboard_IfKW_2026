64f291d7416cd13509bff3924485a9850701530e
martinmfischer <mmartinf@Silencer.localdomain>
2026-03-03 16:45:43 +0100

    - added Renv support to make the project more reproducible - added all required linux libraries to .sh file - moved main app to ChatDashboard-app folder

---
+++ install-linux-packages.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install -y \
+  build-essential cmake gfortran pkg-config \
+  libgdal-dev libgeos-dev libproj-dev libudunits2-dev \
+  libssl-dev libcurl4-openssl-dev libxml2-dev \
+  libpng-dev libjpeg-dev libharfbuzz-dev libfribidi-dev libmagick++-dev \
+  libblas-dev liblapack-dev \
+  default-jre default-jdk
